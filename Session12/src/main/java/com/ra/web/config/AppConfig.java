123760222330301a65128e46f048ad9589df773c
Hoàng Văn Trung <trunghv@bachkhoa-aptech.edu.vn>
2023-09-23 10:30:14 +0700

    Session12: Thymeleaf + Spring MVC

---
+++ Session12/src/main/java/com/ra/web/config/AppConfig.java
@@ -0,0 +1,87 @@
+package com.ra.web.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.ra.web"})
+public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
+    private ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        localeResolver.setLocaleAttributeName("session.current.locale");
+        localeResolver.setTimeZoneAttributeName("session.current.timezone");
+        return localeResolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("language");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(templateResolver());
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+        return viewResolver;
+    }
+}
---
+++ Session12/src/main/java/com/ra/web/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.ra.web.controller;
+
+import com.ra.web.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    private List<User> data;
+
+    public HomeController() {
+        this.data = new ArrayList<>(Arrays.asList(
+                new User("admin1", "0987654321", true),
+                new User("admin2", "0987654321", false),
+                new User("admin3", "0987654321", true),
+                new User("admin4", "0987654321", false),
+                new User("admin5", "0987654321", true),
+                new User("admin6", "0987654321", false)
+        ));
+    }
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("fullName", "Hoàng Văn Trung");
+        model.addAttribute("user", new User("admin", "0987654321", false));
+        model.addAttribute("data", data);
+        return "home/index";
+    }
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("user", new User("default", "0000000000", true));
+        return "home/create";
+    }
+    @PostMapping("/create")
+    public String create(User user, Model model) {
+        data.add(user);
+        return "redirect:/home";
+    }
+}
